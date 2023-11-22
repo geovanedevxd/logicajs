574c01cdee44adba42c192b3601c2e640bad0ade
geovanedevxd <geoanalistabsb@gmail.com>
2023-11-22 06:17:05 -0300

    first commit

---
+++ src/js/index2.js
@@ -0,0 +1,6 @@
+
+const nome = prompt("Qual o seu nome?")
+const idade = prompt("Qual a sua idade?")
+const linguagem = prompt("Que Linguagem de programação voçe gosta?")
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}! Continue praticando e será um grande desenvolvedor"!`;
+alert(msg);
